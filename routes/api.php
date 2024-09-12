576135fe96dacf5b5896025bcafa3af25b38c3be
Carlos Santos <carlos.santos.dev@gmail.com>
2024-09-11 22:04:13 -0300

    feat: add new routes for api

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
 
+use App\Http\Controllers\InterestController;
+use App\Http\Controllers\SubscribeInterestController;
+use App\Http\Controllers\TouchInterestController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\CustomAuth;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/subscribe/interest', SubscribeInterestController::class);
+Route::post('/touch/{interest}', TouchInterestController::class);
 
 Route::middleware(CustomAuth::class)->group(function () {
     Route::post('/users', [UserController::class, 'store']);
-
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::post('/interests', [InterestController::class, 'store']);
 });
