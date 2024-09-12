2f1566f353ec3554b6abdc5bcd1c13672f25c08b
Carlos Santos <carlos.santos.dev@gmail.com>
2024-09-11 21:59:48 -0300

    feat: add models Interest and InterestSubscription. Update User model with relationship.

---
+++ app/Models/InterestSubscription.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Notifications\Notifiable;
+
+class InterestSubscription extends Model
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'interest_id',
+        'email',
+    ];
+
+    public function interest(): BelongsTo
+    {
+        return $this->belongsTo(Interest::class);
+    }
+}
