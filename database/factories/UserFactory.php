e240ba0186da7a9efb4f71b00c7a5867aa17d2fc
Carlos Santos <carlos.santos.dev@gmail.com>
2024-09-11 22:00:05 -0300

    feat: update UserFactory with token

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'token' => fake()->text(16),
             'is_admin' => false,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
